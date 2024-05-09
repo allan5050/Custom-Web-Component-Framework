5e794c7f71f761f6084fe974956621ed9a178644
allan5050 <58089881+allan5050@users.noreply.github.com>
2024-05-09 17:47:12 +0300

    v1 simple-counter stable final.

--- your-framework.js
+++ your-framework.js
@@ -1,43 +1,50 @@
 // your-framework.js
 export function define(component) {
-  const { tag, render, ...initialState } = component;
+    const { tag, render, ...initialState } = component;
 
-  class CustomElement extends HTMLElement {
-    constructor() {
-      super();
-      Object.assign(this, initialState);
-      this.attachShadow({ mode: 'open' });
-      this.updateComponent();
-    }
+    class CustomElement extends HTMLElement {
+        constructor() {
+            super();
+            Object.assign(this, initialState);
+            this.attachShadow({ mode: 'open' });
+            this.updateComponent();
+        }
 
-    updateComponent() {
-      const template = document.createElement('template');
-      template.innerHTML = render(this);
-      this.shadowRoot.innerHTML = '';
-      this.shadowRoot.appendChild(template.content.cloneNode(true));
-      this.bindEventListeners();
-    }
+        updateComponent() {
+            const template = document.createElement('template');
+            template.innerHTML = render(this);
+            this.shadowRoot.innerHTML = '';
+            this.shadowRoot.appendChild(template.content.cloneNode(true));
+            this.bindEventListeners();
+            this.updateAttributes();
+        }
 
-    bindEventListeners() {
-      const eventElements = this.shadowRoot.querySelectorAll('[onclick]');
-      eventElements.forEach(element => {
-        const eventHandler = new Function('event', `
+        updateAttributes() {
+            Object.keys(initialState).forEach(attr => {
+                this.setAttribute(attr, this[attr]);
+            });
+        }
+
+        bindEventListeners() {
+            const eventElements = this.shadowRoot.querySelectorAll('[onclick]');
+            eventElements.forEach(element => {
+                const eventHandler = new Function('event', `
           with (this) {
             (${element.getAttribute('onclick')})(this);
             this.updateComponent();
           }
         `).bind(this);
-        element.addEventListener('click', eventHandler);
-      });
+                element.addEventListener('click', eventHandler);
+            });
+        }
     }
-  }
 
-  customElements.define(tag, CustomElement);
+    customElements.define(tag, CustomElement);
 }
 
 export function html(strings, ...values) {
-  return strings.reduce((result, string, i) => {
-    const value = values[i] ? values[i] : '';
-    return result + string + value;
-  }, '');
+    return strings.reduce((result, string, i) => {
+        const value = values[i] ? values[i] : '';
+        return result + string + value;
+    }, '');
 }
